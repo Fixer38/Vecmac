4107df9d6dc47d5152393c64a6f849c227943fd9
TheWolfInstinct <thunderbear38@gmail.com>
2020-11-29 21:48:01 +0100

    ADDED trailing expression inside macro and test associated to it

--- src/lib.rs
+++ src/lib.rs
@@ -4,10 +4,14 @@ macro_rules! vecmac {
         Vec::new()
     };
 
-    ($($element:expr), +) => {{
+    // First pattern is for as many elements as we want to init the vec
+    // Second pattern to avoid the removal of coma in case there is trailing
+    ($($element:expr), + $(,)*) => {{
         let mut vs = Vec::new();
         // Repeat the pattern as many times as element is in it
         // Looks for the number of time the pattern element is in it
+        // If multiple variables, requestion is that they must repeat the same amount of time
+        // + = 0 or more, * = 1 or more
         $(vs.push($element);)+
         vs
     }};
@@ -31,6 +35,15 @@ fn single() {
 #[test]
 fn double() {
     let x: Vec<u32> = vecmac![35, 41];
+    assert_eq!(x.len(), 2);
+    assert_eq!(x[0], 35);
+    assert_eq!(x[1], 41);
+}
+
+// Allow trailing coma at the end of a long list of elements
+#[test]
+fn trailing() {
+    let x: Vec<u32> = vecmac![35, 41,];
     assert_eq!(x.len(), 2);
     assert_eq!(x[0], 35);
     assert_eq!(x[1], 41);
