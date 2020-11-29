f6d0ae037344a88a9bbd12f158f3902c56b5ba0d
TheWolfInstinct <thunderbear38@gmail.com>
2020-11-29 22:10:14 +0100

    REMOVED empty pattern REPLACED in the first pattern with a proc macro to allow unusued mutable

--- src/lib.rs
+++ src/lib.rs
@@ -1,18 +1,15 @@
 #[macro_export]
 macro_rules! vecmac {
-    () => {
-        Vec::new()
-    };
-
     // First pattern is for as many elements as we want to init the vec
     // Second pattern to avoid the removal of coma in case there is trailing
-    ($($element:expr), + $(,)*) => {{
+    ($($element:expr),* $(,)*) => {{
+        #[allow(unused_mut)]
         let mut vs = Vec::new();
         // Repeat the pattern as many times as element is in it
         // Looks for the number of time the pattern element is in it
         // If multiple variables, requestion is that they must repeat the same amount of time
         // + = 0 or more, * = 1 or more
-        $(vs.push($element);)+
+        $(vs.push($element);)*
         vs
     }};
 
