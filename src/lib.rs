55e8513054324eef8612f44db4ca2f382510509e
TheWolfInstinct <thunderbear38@gmail.com>
2020-11-29 21:29:12 +0100

    ADDED expression to have a single element in a macro and the test for it

--- src/lib.rs
+++ src/lib.rs
@@ -1,8 +1,14 @@
 #[macro_export]
 macro_rules! vecmac {
     () => {
-        Vec::new();
-    }
+        Vec::new()
+    };
+
+    ($element:expr) => {{
+        let mut vs = Vec::new();
+        vs.push($element);
+        vs
+    }};
 }
 
 
@@ -11,4 +17,11 @@ macro_rules! vecmac {
 fn empty_vec() {
     let x: Vec<u32> = vecmac![];
     assert!(x.is_empty());
+}
+
+#[test]
+fn single() {
+    let x: Vec<u32> = vecmac![35];
+    assert_eq!(x.len(), 1);
+    assert_eq!(x[0], 35);
 }
